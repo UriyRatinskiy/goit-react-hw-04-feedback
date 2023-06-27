20d4923827bf085e193f991815918f3436f19874
UriyRatinskiy <uriy_mark@ukr.net>
2023-06-27 23:58:10 +0300

    hw04

--- src/components/AppByHooks.jsx
+++ src/components/AppByHooks.jsx
@@ -4,59 +4,92 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-const AppByHooks = () => {
-  const [feedbacks, setFeedbacks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+const Feedback = () => {
+  //   const [feedbacks, setFeedbacks] = {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleCountFeedbacks = statusFeedback => {
-    setFeedbacks(prevState => ({
-      ...prevState,
-      [statusFeedback]: prevState[statusFeedback] + 1,
-    }));
+  // good: 0,
+  // neutral: 0,
+  // bad: 0,
+  const handleCountFeedbacks = e => {
+    // e.preventDefault();
+    const { name } = e.target;
+
+    switch (name) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      default:
+        return undefined;
+    }
   };
 
+  // statusFeedback => {
+  //   setFeedbacks(prevState => ({
+  //     ...prevState,
+  //     [statusFeedback]: prevState[statusFeedback] + 1,
+  //   }));
+  // };
+
   const countTotalFeedback = () => {
-    const values = Object.values(feedbacks);
-    let total = 0;
-    for (const value of values) {
-      total += value;
-    }
-    return total;
+    return good + neutral + bad;
+    // const values = Object.values(feedbacks);
+    // let total = 0;
+    // for (const value of values) {
+    //   total += value;
+    // }
+    // return total;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    const positiveFeedbackPercentage = Math.round(
-      (feedbacks.good * 100) / countTotalFeedback()
-    );
-    return feedbacks.good ? positiveFeedbackPercentage : 0;
+    const total = good + neutral + bad;
+    const positive = Math.round((good / total) * 100);
+    return positive ? positive : 0;
+    // const positiveFeedbackPercentage = Math.round(
+    //   (feedbacks.good * 100) / countTotalFeedback()
+    // );
+    // return feedbacks.good ? positiveFeedbackPercentage : 0;
   };
-  const optionsForFeedback = Object.keys(feedbacks);
+  // const optionsForFeedback = Object.keys(feedbacks);
+  const total = countTotalFeedback();
+  const positive = countPositiveFeedbackPercentage();
 
   return (
     <>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={optionsForFeedback}
+          //   options={optionsForFeedback}
+
+          // />
+          options={['good', 'neutral', 'bad']}
           onLeaveFeedback={handleCountFeedbacks}
-        />
+          // onLeaveFeedback={onButtonClick}
+        ></FeedbackOptions>
       </Section>
 
       <Section title="Statistics">
-        {countTotalFeedback() === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
+        {total > 0 ? (
           <Statistics
-            feedbacks={feedbacks}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
-          />
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positive}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
         )}
       </Section>
     </>
   );
 };
 
-export default AppByHooks;
+export default Feedback;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/AppByHooks';
+import Feedback from 'components/AppByHooks';
 import './index.css';
 import { theme } from './constants/theme';
 import { ThemeProvider } from 'styled-components';
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'styled-components';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Feedback />
     </ThemeProvider>
   </React.StrictMode>
 );
